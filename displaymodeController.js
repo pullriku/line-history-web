8c28ca004fc456b96fa599a9ad111a93ab2b1ecf
TyomoGit <riku3dno.1@gmail.com>
2022-11-20 13:26:41 +0900

    versions表示中に表示モードの切り替えをサポート

--- displaymodeController.js
+++ displaymodeController.js
@@ -1,6 +1,9 @@
 var darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 var isLightMode = !darkModeMediaQuery.matches;
 setDisplayMode(isLightMode);
+darkModeMediaQuery.addEventListener("change", function (e) {
+    setDisplayMode(!darkModeMediaQuery.matches);
+});
 function setDisplayMode(isLightMode) {
     if (isLightMode) {
         document.documentElement.setAttribute("theme", "light");
--- displaymodeController.ts
+++ displaymodeController.ts
@@ -2,6 +2,10 @@ const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 let isLightMode = ! darkModeMediaQuery.matches;
 setDisplayMode(isLightMode);
 
+darkModeMediaQuery.addEventListener("change", (e)=>{
+    setDisplayMode( ! darkModeMediaQuery.matches);
+})
+
 function setDisplayMode(isLightMode: boolean): void{
     if(isLightMode){
         document.documentElement.setAttribute("theme", "light");
